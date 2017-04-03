3b100e64b93bf1b459bd724de4a8bb67529488fe
Chris M. <chris.perez@upr.edu>
2017-04-03 13:14:26 -0400

    new commit

---
+++ src/ThirtyDayOfCode/Day0HelloWorld.java
@@ -0,0 +1,27 @@
+package ThirtyDayOfCode;
+import java.util.Scanner;
+
+/*
+    Problem: Day 0: Hello,World
+
+    Input Format:
+        A single line of text denoting inputString (the variable whose contents must be printed).
+
+    Output Format:
+        Print Hello, World. on the first line, and the contents of inputString on the second line.
+
+    @Author Chris M. Perez
+    @Date  4/3/2017
+*/
+
+public class Day0HelloWorld {
+    static Scanner console = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        String world;
+        
+        world = console.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(world);
+    }
+}
